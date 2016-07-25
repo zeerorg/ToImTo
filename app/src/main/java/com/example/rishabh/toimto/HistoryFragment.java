4c29969a5338739799ea0860bdea2fdafd3041f3
zeerorg <r.g.gupta@outlook.com>
2016-07-25 13:13:26 +0530

    Navigation Drawer added

---
+++ app/src/main/java/com/example/rishabh/toimto/HistoryFragment.java
@@ -0,0 +1,52 @@
+package com.example.rishabh.toimto;
+
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.rishabh.toimto.Utilities.VideoContract;
+import com.example.rishabh.toimto.Utilities.VideoDbHelper;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HistoryFragment extends Fragment {
+
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Log.e("History Fragment", "Started");
+        VideoDbHelper dbHelper = new VideoDbHelper(getContext());
+        View v = inflater.inflate(R.layout.fragment_history, container, false);
+        TextView dbString = (TextView) v.findViewById(R.id.history);
+        String result = "";
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + VideoContract.VideoEntry.TABLE_NAME, null);
+        while (cursor.moveToNext()) {
+            result += cursor.getString(cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_TITLE));
+            result += " : " + cursor.getInt(cursor.getColumnIndex(VideoContract.VideoEntry.COLUMN_DATE_TIME));
+            result += "\n";
+        }
+        db.close();
+        cursor.close();
+        dbString.setText(result);
+
+        return v;
+    }
+
+}
--- app/src/main/java/com/example/rishabh/toimto/MainActivity.java
+++ app/src/main/java/com/example/rishabh/toimto/MainActivity.java
@@ -1,39 +1,81 @@
 package com.example.rishabh.toimto;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    String[] listContent;
+    DrawerLayout drawer;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_container);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        listContent = new String[] {"Movie", "TV Shows", "Actors"};
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.nav_open, R.string.nav_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.drawer_navigation);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        int id = item.getItemId();
+                        selectItem(id);
+                        item.setChecked(true);
+                        drawer.closeDrawers();
+                        return true;
+                    }
+                }
+        );
+        Fragment fragment = new MainActivityFragment();
+        Bundle args = new Bundle();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
         return true;
     }
 
@@ -47,8 +89,40 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == android.R.id.home) {
+            drawer.openDrawer(GravityCompat.START);
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer != null && drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void selectItem(int i){
+
+        if (i == R.id.nav_history){
+            Fragment fragment = new HistoryFragment();
+            Bundle args = new Bundle();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+        } else {
+            Fragment fragment = new MainActivityFragment();
+            Bundle args = new Bundle();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame, fragment)
+                    .commit();
+        }
+//        listView.setItemChecked(i, true);
+//        drawer.closeDrawer(listView);
     }
 }
